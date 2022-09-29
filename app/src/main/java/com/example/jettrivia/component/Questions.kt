b9ad25f2955517ae368b54985c0dd12f156a13f0
joey sparidaans <jsparidaans@gmail.com>
2022-09-29 15:55:21 +0200

    created a background with a tracker for questions

--- app/src/main/java/com/example/jettrivia/component/Questions.kt
+++ app/src/main/java/com/example/jettrivia/component/Questions.kt
@@ -1,21 +1,89 @@
 package com.example.jettrivia.component
 
-import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jettrivia.screens.QuestionsViewModel
+import com.example.jettrivia.util.AppColors
 
 
 @Composable
 fun Questions(viewModel: QuestionsViewModel) {
     val questions = viewModel.data.value.data?.toMutableList()
-    if(viewModel.data.value.loading == true){
+    if (viewModel.data.value.loading == true) {
         CircularProgressIndicator()
-        Log.d("Load", "Questions: Loading...")
-    }else{
-        Log.d("Load", "Questions: Loading complete!")
+
+    } else {
+
         questions?.forEach { questionItem ->
-            Log.d("Result", "Questions: ${questionItem.question}")
+
         }
     }
+}
+
+@Preview
+@Composable
+fun QuestionDisplay() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp),
+        color = AppColors.mDarkPurple
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            QuestionTracker()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
+    Text(
+        modifier = Modifier.padding(20.dp),
+        text = buildAnnotatedString {
+            withStyle(
+                style = ParagraphStyle(textIndent = TextIndent.None)
+            ) {
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 27.sp
+                    )
+                ) {
+                    append("Question $counter/")
+                    withStyle(
+                        style = SpanStyle(
+                            color = AppColors.mLightGray,
+                            fontWeight = FontWeight.Light,
+                            fontSize = 14.sp
+                        )
+                    ) {
+                        append("$outOf")
+                    }
+                }
+            }
+        }
+    )
 }
