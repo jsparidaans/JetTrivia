12390efd9d01343e7a2aaf07aacf81abc2f14afd
joey sparidaans <jsparidaans@gmail.com>
2022-09-29 15:40:35 +0200

    added fetching of questions

--- app/src/main/java/com/example/jettrivia/MainActivity.kt
+++ app/src/main/java/com/example/jettrivia/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jettrivia
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import com.example.jettrivia.screens.QuestionsViewModel
 import com.example.jettrivia.ui.theme.JetTriviaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TriviaHome()
                 }
             }
         }
@@ -30,14 +36,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TriviaHome(viewModel: QuestionsViewModel= hiltViewModel()) {
+    Questions(viewModel)
+
+}
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+    if(viewModel.data.value.loading == true){
+        Log.d("Load", "Questions: Loading...")
+    }else{
+        Log.d("Load", "Questions: Loading complete!")
+        questions?.forEach { questionItem ->
+            Log.d("Result", "Questions: ${questionItem.question}")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetTriviaTheme {
-        Greeting("Android")
     }
 }
